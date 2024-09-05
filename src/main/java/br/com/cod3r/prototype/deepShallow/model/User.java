67c846df38aaa544ad87edc291a0f3fd52b64ee3
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2024-09-05 17:59:44 -0300

    Implementação => Exemplos do padrão prototype.

--- src/main/java/br/com/cod3r/prototype/deepShallow/model/User.java
+++ src/main/java/br/com/cod3r/prototype/deepShallow/model/User.java
@@ -19,6 +19,12 @@ public class User implements Cloneable {
 	
 	@Override
 	public User clone() throws CloneNotSupportedException {
-		return (User) super.clone();
+		//Error
+//		return (User) super.clone();
+		
+		//Solution
+		User cloneUser = (User) super.clone();
+		cloneUser.address = (Address) cloneUser.address.clone();
+		return cloneUser;
 	}
 }
